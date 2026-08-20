74cdd1c66b43ec218fe841f8dbe798164cad62f7
Willem van Heemstra <wvanheemstra@users.noreply.github.com>
2026-08-20 14:37:34 +0200

    Delete doc/architecture/src/hooks.server.ts

--- doc/architecture/src/hooks.server.ts
+++
@@ -1,7 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-import { getTracingConfig } from '$lib/observability';
-
-export const handle: Handle = async ({ event, resolve }) => {
-	(event.locals as any).otel = getTracingConfig();
-	return resolve(event);
-};
